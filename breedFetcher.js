21c64ec93339cd57d5c5d28aff9557d0d940787e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 16:13:11 -0700

    Added code logic to retrieve breed info for cat breed specified by user as a CL argument.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,3 +1,18 @@
-const net = require("net");
-
 const request = require("request");
+
+let userInput = process.argv.slice(2);
+// console.log(userInput);
+
+request(
+  "https://api.thecatapi.com/v1/breeds/search?q=" + userInput,
+  (error, response, body) => {
+    if (error) {
+      console.log("There was an error while fetching: ", error);
+      return;
+    }
+    const data = JSON.parse(body);
+    // console.log(data);
+    const catDescription = data[0].description;
+    console.log(catDescription);
+  }
+);
