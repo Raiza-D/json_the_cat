9ee82806931a2846f6c7f1099b5e5d9071e12246
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 17:29:38 -0700

    Cleaned up messy comments and other unnecessary lines of code. Added correct code logic to take care of no breed input as an argument on CL or if breed provided not found. Code still works as it should.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,23 @@
 const request = require("request");
 
 let userInput = process.argv.slice(2);
-// console.log(userInput);
 
-// let catApiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+let catApiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + userInput;
-// this works -- request("https://api.thecatapi.com/v1/breeds/search?q=" + userInput, (error));
-
-// `${catApiUrl}${userInput}`
-
-request(URL, (error, response, body) => {
-    if (error) {
-      console.log("There was an error while fetching: ", error);
-      return;
-    }
-    // if (!URL) {
-    //   console.log("Breed name not found. Enter another one.");
-    //   return;
-    // }
-    const data = JSON.parse(body);
-
-    if (data[0] === undefined) {
-      console.log("Error found");
-      return;
-    }
-    // console.log(data);
-    const catDescription = data[0].description;
-    console.log(catDescription);
+request(`${catApiUrl}${userInput}`, (error, response, body) => {
+  // Code for taking care of error if API is not working
+  if (error) {
+    console.log("There was an error while fetching: ", error);
+    return;
   }
-);
+  const data = JSON.parse(body);
+
+  // Code to take care of if cat breed provided not found OR if none provided
+  if (data[0] === undefined) {
+    console.log(`Breed not found: ${data[0]}. Enter a valid breed.`);
+    return;
+  }
+
+  const catDescription = data[0].description;
+  console.log(catDescription);
+});
