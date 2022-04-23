0245f36c0b6d85a043e04292226f6821eadfd146
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 19:37:54 -0700

    Removed commented notes to clean up file.

--- index.js
+++ index.js
@@ -9,12 +9,3 @@ fetchBreedDescription(breedName, (error, desc) => {
     console.log(desc);
   }
 });
-
-/* As per mentor:
-If you want to use and print the console log messages of within if statements
-in the breedFetcher.js file, you'd have to remove the second parameter for
-fetchBreedDescription function. This leaves it with only one argument -- the breedName.
-How I had it before was not technically wrong. It's just not the right way for
-this assignment. Assignment requests to use the callback function of
-fetchBreedDescription.
-*/
