0726e21b058f70a61a9e166c2fa744ff1cd2d9a5
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 19:29:21 -0700

    Implemented code to call the callback second parameter for fetchBreedDescription. Commented out my console log's within both if statements. Added null values for callback's second param of 'desc' (the description) since there's no value for it when there's an error that is found.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,28 @@
 const request = require("request");
 
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
-
 const fetchBreedDescription = function(breedName, callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
   request(URL, (error, response, body) => {
-    // Code for taking care of error if API is not working
+    // // Code for taking care of error if API is not working
     if (error) {
-      console.log("There was an error while fetching: ", error);
+      // console.log("There was an error while fetching: ", error);
+      callback(error, null);
       return;
     }
     const data = JSON.parse(body);
 
     // Code to take care of if cat breed provided not found OR if none provided
     if (data[0] === undefined) {
-      console.log(`Breed not found: ${data[0]}. Enter a valid breed.`);
+      // console.log(`Breed not found: ${data[0]}. Enter a valid breed.`);
+      callback(`${error} Breed not found: ${data[0]}. Enter a valid breed.`, null);
       return;
     }
 
     const catDescription = data[0].description;
-    console.log(catDescription);
-  })
+    // console.log(catDescription);
+    callback(null, catDescription);
+  });
 };
 
 module.exports = { fetchBreedDescription };
