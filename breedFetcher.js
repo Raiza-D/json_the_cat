8e39e99dd95d5b4f37a728057f0c3ad7dba6f85d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 17:15:09 -0700

    Added commented lines of code and new code lines during session with mentor. Will clean up later.

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,16 +3,28 @@ const request = require("request");
 let userInput = process.argv.slice(2);
 // console.log(userInput);
 
-let catApiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+// let catApiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-request(
-   `${catApiUrl}${userInput}`,
-  (error, response, body) => {
+const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + userInput;
+// this works -- request("https://api.thecatapi.com/v1/breeds/search?q=" + userInput, (error));
+
+// `${catApiUrl}${userInput}`
+
+request(URL, (error, response, body) => {
     if (error) {
       console.log("There was an error while fetching: ", error);
       return;
     }
+    // if (!URL) {
+    //   console.log("Breed name not found. Enter another one.");
+    //   return;
+    // }
     const data = JSON.parse(body);
+
+    if (data[0] === undefined) {
+      console.log("Error found");
+      return;
+    }
     // console.log(data);
     const catDescription = data[0].description;
     console.log(catDescription);
