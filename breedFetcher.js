8bde9cc067dc18cc8b437432e7abac695f10102f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 19:33:20 -0700

    Added commented notes from session with mentor for future reference.

--- breedFetcher.js
+++ breedFetcher.js
@@ -27,4 +27,12 @@ const fetchBreedDescription = function(breedName, callback) {
 
 module.exports = { fetchBreedDescription };
 
+/* As per mentor:
+If you want to use and print the console log messages of within if statements
+in the breedFetcher.js file, you'd have to remove the second parameter for
+fetchBreedDescription function. This leaves it with only one argument -- the breedName.
+How I had it before was not technically wrong. It's just not the right way for
+this assignment. Assignment requests to use the callback function of
+fetchBreedDescription.
+*/
 
