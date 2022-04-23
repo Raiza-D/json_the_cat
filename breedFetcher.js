f5dd02b641fa8447277cfd6e4a9528d5d1d64c75
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-22 18:19:10 -0700

    Declared URL variable containing URL and userInput data as the argument entered on the command line to clean up code. Added fetchBreedDescription function.This function houses the request function. Added module exports code to export fetchBreedDescription function.

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,22 +2,29 @@ const request = require("request");
 
 let userInput = process.argv.slice(2);
 
-let catApiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+let URL = "https://api.thecatapi.com/v1/breeds/search?q=" + userInput;
 
-request(`${catApiUrl}${userInput}`, (error, response, body) => {
-  // Code for taking care of error if API is not working
-  if (error) {
-    console.log("There was an error while fetching: ", error);
-    return;
-  }
-  const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
 
-  // Code to take care of if cat breed provided not found OR if none provided
-  if (data[0] === undefined) {
-    console.log(`Breed not found: ${data[0]}. Enter a valid breed.`);
-    return;
-  }
+  request(URL, (error, response, body) => {
+    // Code for taking care of error if API is not working
+    if (error) {
+      console.log("There was an error while fetching: ", error);
+      return;
+    }
+    const data = JSON.parse(body);
 
-  const catDescription = data[0].description;
-  console.log(catDescription);
-});
+    // Code to take care of if cat breed provided not found OR if none provided
+    if (data[0] === undefined) {
+      console.log(`Breed not found: ${data[0]}. Enter a valid breed.`);
+      return;
+    }
+
+    const catDescription = data[0].description;
+    console.log(catDescription);
+  })
+};
+
+module.exports = { fetchBreedDescription };
+
+
